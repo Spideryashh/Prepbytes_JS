26005b5cde9dfeed33bd0936eebfc8750eda2e8a
Yash <yash17.student@gmail.com>
2021-12-17 11:13:10 +0530

    assignment update

---
+++ assignments/Assignment2.js
@@ -0,0 +1,28 @@
+// Q1 => Reverse an array 
+reverse = (arr)=>{
+    let reverseArrayToReturn = []
+    for(let i=arr.length-1;i>=0;i--){ 
+        reverseArrayToReturn[i] =  arr[arr.length-1-i] 
+    }
+    return reverseArrayToReturn;
+}
+let inputArray = [1,2,3,44,55]
+let inputArray2 = [22,33,0,1]
+console.log(reverse(inputArray));
+console.log(reverse(inputArray2));
+
+//----------------------------------------------------------
+
+//Q2 => Reverse the string
+reverseString =(str)=>{
+    let reverseStrToReturn = ''
+    for(let i=str.length-1;i>=0;i--){
+        reverseStrToReturn+=str.charAt(i);
+    }
+    return reverseStrToReturn;
+}
+
+let str = "students";
+let str2 = "userdata";
+console.log(reverseString(str));
+console.log(reverseString(str2));
